f2e1ebb8dc25b9d2f8dfcb4e03cefb64a964b0c9
Ilkka Kosunen <ilkka.kosunen@helsinki.fi>
2017-01-28 11:10:31 +0200

    added the apikey

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyCH2z_8iMqwouNLAyOaklhp3aQE9rvFdR0';
 import SearchBar from './components/search_bar';
 // Create new component.
 const App = () => {
