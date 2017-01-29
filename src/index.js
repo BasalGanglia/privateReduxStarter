425307b97b1aaf753b1b8ede0bb924da2bbc9b25
Ilkka Kosunen <ilkka.kosunen@helsinki.fi>
2017-01-29 11:06:07 +0200

    changed App from function to class

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyCH2z_8iMqwouNLAyOaklhp3aQE9rvFdR0';
 import SearchBar from './components/search_bar';
+
+YTSearch({key:API_KEY, term:'surfboards'}, function(data){
+    console.log(data);
+});
+
 // Create new component.
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component{
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it in the DOM
